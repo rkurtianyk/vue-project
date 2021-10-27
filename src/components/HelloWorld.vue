c2fa6b2becc29e0b350f5c63ab217ccf71a2bf35
Ruslan <ruslan.kurtianyk@gonewfire.com>
2021-10-27 18:23:59 +0300

    init

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,38 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  msg: String
+})
+
+defineEmits(['someCustomEmit'])
+
+const count = ref(0)
+const message = ref('');
+
+const showResult = () => {
+  console.log(message)
+}
+
+</script>
+
+<template>
+<div>
+    <h1>{{ msg }}</h1>
+
+  <p>
+    <input type="text" v-model="message"/>
+  </p>
+
+  <button type="button" @click="$emit('someCustomEmit', 'test')">Press Me!</button>
+
+  <p>{{ message }}</p>
+</div>
+
+</template>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+</style>
